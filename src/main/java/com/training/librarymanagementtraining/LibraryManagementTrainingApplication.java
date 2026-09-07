59f181fe2b2ee7c2fd554fb16096f6afe7b36cbc
Nellaiappan-ig19 <nellai1661@gmail.com>
2026-09-07 12:15:50 +0530

    Create base Spring Boot project

---
+++ src/main/java/com/training/librarymanagementtraining/LibraryManagementTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.training.librarymanagementtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/librarymanagementtraining/LibraryManagementTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.librarymanagementtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
